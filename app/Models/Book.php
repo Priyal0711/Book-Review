3dcd6971fb6a5dbe07219f047b1daad40934fc7b
Priyal0711 <priyal.chaudhari@brainvire.com>
2023-09-11 18:26:55 +0530

    updated code

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder as QueryBuilder;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,5 +14,47 @@ class Book extends Model
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
+    }
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder|QueryBuilder
+    {
+        
+        return $query->withCount([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ]);
+    }
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder|QueryBuilder
+    {
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
+    }
+    public function scopeWithRecentReviews(Builder $query, \Closure $interval): Builder
+    {
+       
+        return $query->whereHas(
+            'reviews',
+            function (Builder $q) use ($interval) {
+                $q->whereBetween(
+                    'created_at',
+                    [$interval(now()), now()]
+                );
+            }
+        );
+
+    private function dateRangeFilter(Builder $query, $from = null, $to = null)
+    {
+       
+        if ($from && !$to) {
+            $query->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            $query->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            $query->whereBetween('created_at', [$from, $to]);
+        }
     }
 }
